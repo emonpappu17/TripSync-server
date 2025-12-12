3739b99c67f6b35f6cf843fabd4f5904f4b4fabd
emonpappu17 <emon.pappu.5@gmail.com>
2025-12-12 09:14:14 +0600

    add: render deploy

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-bun install
-bun run build
-bunx prisma generate 
-bunx prism migrate deploy
+npm install
+npm run build
+npx prisma generate
+npx prisma migrate deploy
