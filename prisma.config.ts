25db46963fe0ac3cce7dfeb25e27ce1a34cdb3d2
emonpappu17 <emon.pappu.5@gmail.com>
2025-12-12 20:33:47 +0600

    fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,23 @@
+// import { defineConfig, env } from "@prisma/config";
+// import "dotenv/config";
+
+// export default defineConfig({
+//   schema: "prisma/schema.prisma",
+//   // schema: "prisma/schema",
+//   migrations: {
+//     path: "prisma/migrations",
+//   },
+//   datasource: {
+//     url: env("DATABASE_URL"),
+//   },
+// });
+
+
 import { defineConfig, env } from "@prisma/config";
 import "dotenv/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  // schema: "prisma/schema",
+  schema: "prisma/schema/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
