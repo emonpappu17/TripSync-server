88e5022b26657f5bf64c6803250e6ba569976a26
emonpappu17 <emon.pappu.5@gmail.com>
2025-12-12 20:17:21 +0600

    fix: deploy

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,8 @@ import { defineConfig, env } from "@prisma/config";
 import "dotenv/config";
 
 export default defineConfig({
-  schema: "prisma/schema",
+  schema: "prisma/schema.prisma",
+  // schema: "prisma/schema",
   migrations: {
     path: "prisma/migrations",
   },
